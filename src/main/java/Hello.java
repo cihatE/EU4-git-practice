2931e5edba5949a68a216ea481cc176b8c4e9fed
Cihat Eminsoy <cihateminsoy@gmail.com>
2021-02-12 15:14:48 +0800

    Movie 1 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World , EU4");
         System.out.println("EU4 future SDET learing Git");
     }
 
