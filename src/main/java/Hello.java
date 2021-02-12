d8e5394d7b1d0e3d3a8002cff041bf39c4b7b9aa
Cihat Eminsoy <cihateminsoy@gmail.com>
2021-02-12 17:30:31 +0800

    MASTER branch watching in the living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
         System.out.println("Hello World , AU4");
         System.out.println("EU4 future SDET learing Git");
 
-        //Ohh! I like this movie who played it? Let me continue
+        //Ohh! I like this movie who played it? Let me continue, UPDATED THIS LINE!!!!
     }
 
 
